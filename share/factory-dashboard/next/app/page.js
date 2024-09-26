8eb78858bfb36e6e3f0553a23ad706c59323601e
LOPEZ Cédric <cedric.alyfe.co>
2024-09-26 15:05:39 +0200

    dashboard testing

--- share/factory-dashboard/next/app/page.js
+++ share/factory-dashboard/next/app/page.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { execCommand } from "./services/factoryService";
 
 export default async function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
+  const allContainers = await execCommand('docker ps -a');
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
       <Link className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600" href={'/install'}><button>install</button></Link>
 
     </main>
